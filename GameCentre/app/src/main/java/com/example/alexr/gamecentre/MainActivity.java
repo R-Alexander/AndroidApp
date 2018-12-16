839ef299c3de3dc165196566774fd6a4e74f8aa1
Alexander Rabczak <rabczakalex@gmail.com>
2018-12-15 20:42:36 -0500

    Update MainActivity.java

--- GameCentre/app/src/main/java/com/example/alexr/gamecentre/MainActivity.java
+++ GameCentre/app/src/main/java/com/example/alexr/gamecentre/MainActivity.java
@@ -24,6 +24,6 @@ public class MainActivity extends Activity {
         v.setLayoutParams(p1);
         v.setY(100);
         v.setX(200);
-        v.setText("Seeing a change on github");
+        v.setText("Testing");
     }
 }
